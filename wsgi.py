65c5c77005b5a6b412f0e94bde7aa395c2321eb3
wangzhen <wang765256860@gmail.com>
2018-10-07 14:05:14 +0800

    modify wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
 from app import create_app
+from dotenv import load_dotenv
 import os
 
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+app = create_app('production')
